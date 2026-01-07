325d475feea555163c6f815244f3b31e01b238de
korochi321 <korochi321@gmail.com>
2026-01-07 22:45:00 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
-  resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
-  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
