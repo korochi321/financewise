cdba57194d22ff953dab4b9f986f489b625401c3
korochi321 <korochi321@gmail.com>
2026-01-07 21:36:35 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    sourcemap: false
   }
 })
